4bf112c9cddaf0d365e93c2651fab643f233d017
ZioulePang <93811399+ZioulePang@users.noreply.github.com>
2023-08-16 19:18:27 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 unsigned int loadTexture(const char* path);
 unsigned int loadCubemap(vector<std::string> faces);
+glm::mat4 calculateLightSpaceMatrix(glm::vec3 lightPosition, glm::vec3 lightDirection);
 
 // settings
 const unsigned int SCR_WIDTH = 1800;
@@ -43,6 +44,18 @@ float translation_x = 0, translation_y = 0;
 float square_num = 0, square_scale = 0;
 float specular_scale = 0;
 
+bool normalShading = false;
+bool toonShading = true;
+bool isCharacter = true;
+float model_x = 0, model_y = 0, model_z = 0;
+float anis = 0.4, sharp = 0.4;
+float wx = 0, wy = 0;
+float theta_r = 0;
+float r = 0;
+float G = 0;
+float d = 0;
+float bloomexp = 1.0f;
+float bloommuti = 1.0f;
 int main()
 {
     // glfw: initialize and configure
@@ -96,14 +109,17 @@ int main()
 
     // configure global opengl state
     glEnable(GL_DEPTH_TEST);
-
+    glEnable(GL_CULL_FACE);
     // build and compile shaders
     Shader shader("./Shader/testcube.glsl", "./Shader/testfragment.glsl");
     Shader roomShader("./Shader/testcube.glsl", "./Shader/testfragment.glsl");
     Shader sceneShader("./Shader/edgeVert.glsl", "./Shader/edgeFrag.glsl");
+    Shader shadowShader("./Shader/shadow_vert.glsl", "./Shader/shadow_frag.glsl");
+    Shader characterShader("./Shader/shadowRig_vert.glsl", "./Shader/shadowRig_frag.glsl");
 
-    Model myModel("./resources/moonbug.obj");
-    Model myRoom("./resources/room/32.fbx");
+    Model myModel("./resources/torso.obj");
+
+    //Model myRoom("./resources/rooms/32.fbx");
 
     float quadVertices[] = { 
         // positions   // texCoords
@@ -134,6 +150,19 @@ int main()
     glGenFramebuffers(1, &framebuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
+   /* unsigned int textureColorbuffer[2];
+    glGenTextures(2, textureColorbuffer);
+    for (unsigned int i = 0; i < 2; i++) 
+    {
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, textureColorbuffer[i], 0);
+    }
+    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
+    glDrawBuffers(2, attachments);*/
+
     unsigned int textureColorbuffer;
     glGenTextures(1, &textureColorbuffer);
     glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
@@ -151,6 +180,44 @@ int main()
         cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+    const unsigned int SHADOW_WIDTH = 1800, SHADOW_HEIGHT = 1600;
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+    // create depth texture
+    unsigned int depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+    GLfloat borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    shader.use();
+    shader.setInt("depthMap", 2);
+
+    roomShader.use();
+    shader.setInt("depthMap", 2);
+
+    glm::vec3  centroid;
+    for (int i = 0; i < myModel.position.size(); i++)
+    {
+        centroid.x += myModel.position[i].x;
+        centroid.y += myModel.position[i].y;
+        centroid.z += myModel.position[i].z;
+    }
+
+    centroid /= myModel.position.size();
+
     // render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -166,11 +233,50 @@ int main()
 
         // render
         // ------
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-        glEnable(GL_DEPTH_TEST);
 
         glClearColor(0.1, 0.1, 0.1, 0.1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glCullFace(GL_FRONT);
+
+        glm::mat4 lightProjection, lightView;
+        glm::mat4 lightSpaceMatrix;
+        float near_plane = 0.1f, far_plane = 100.5f;
+        //lightProjection = glm::ortho(-16.0f, 16.0f, -16.0f, 16.0f, near_plane, far_plane);
+        lightProjection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 1.1f, 150.0f);
+        lightView = glm::lookAt(lightPos,lightPos +  glm::vec3(0.0f,0.0f,-1.0f), glm::vec3(0.0, 1.0, 0.0));
+        lightSpaceMatrix = lightProjection * lightView;
+
+        glm::mat4 characterModel = glm::mat4(1.0f);
+       
+        characterModel = glm::translate(characterModel, glm::vec3(1.0f, -15.0f, -40.0f));
+        //characterModel = glm::rotate(characterModel, glm::radians(-90.0f), glm::vec3(1, 0, 0));
+        //characterModel = glm::rotate(characterModel, glm::radians(-90.0f), glm::vec3(0, 0, 1));
+        characterModel = glm::scale(characterModel, glm::vec3(0.1f));
+
+        shadowShader.use();
+        shadowShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+        shadowShader.setMat4("model", characterModel);
+        // render scene from light's point of view
+        glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+        glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+        glClear(GL_DEPTH_BUFFER_BIT);
+ 
+        myModel.Draw(shadowShader);
+
+        glm::mat4 sceneModel = glm::mat4(1.0f);
+        sceneModel = glm::translate(sceneModel, glm::vec3(1.0f, 0.0f, -67.0f));
+        sceneModel = glm::rotate(sceneModel, glm::radians(280.0f), glm::vec3(1, 0, 0));
+        sceneModel = glm::rotate(sceneModel, glm::radians(180.0f), glm::vec3(0, 0, 1));
+
+        shadowShader.setMat4("model", sceneModel);
+        //myRoom.Draw(shadowShader);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+        glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+       
+        glDisable(GL_CULL_FACE);
 
         shader.use();
         shader.setVec3("myLight.ambient",0.25f,0.25f,0.2f);
@@ -199,14 +305,36 @@ int main()
 
         shader.setFloat("specular_scale", specular_scale);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(1.0f, -20.0f, -40.0f));
+        shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+        shader.setBool("normalShading", normalShading);
+        shader.setBool("toonShading", toonShading);
 
+        shader.setFloat("near_plane", near_plane);
+        shader.setFloat("far_plane", far_plane);
+        shader.setBool("isCharacter", isCharacter);
+
+        shader.setFloat("anis", anis);
+        shader.setFloat("sharp", sharp);
+
+        shader.setFloat("wx", wx);
+        shader.setFloat("wy", wy);
+        shader.setFloat("theta_r", theta_r);
+        shader.setFloat("r", r);
+        shader.setVec3("centroid", centroid);
+        shader.setFloat("Gi", G);
+        shader.setFloat("di", d);
+        shader.setFloat("bloomexp", bloomexp);
+        shader.setFloat("bloommuti", bloommuti);
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        shader.setMat4("model", model);
+        shader.setMat4("model", characterModel);
         shader.setMat4("view", view);
         shader.setMat4("projection", projection);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, depthMap);
+
+        //GLenum drawBuffers[2] = { GL_COLOR_ATTACHMENT0,GL_COLOR_ATTACHMENT1 };
+        //glDrawBuffers(1, drawBuffers);
         myModel.Draw(shader);
 
         roomShader.use();
@@ -216,26 +344,39 @@ int main()
 
         roomShader.setVec3("myLight.lightPos", lightPos);
         roomShader.setVec3("viewPos", camera.Position);
+        roomShader.setVec4("rim", rim);
         roomShader.setFloat("shiness", 64.0f);
+        roomShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+        roomShader.setBool("normalShading", normalShading);
+        roomShader.setBool("toonShading", toonShading);
+        roomShader.setFloat("near_plane", near_plane);
+        roomShader.setFloat("far_plane", far_plane);
 
-        model = glm::translate(model, glm::vec3(0, 20.0f, -27));
-        model = glm::rotate(model, glm::radians(280.0f), glm::vec3(1, 0, 0));
-        model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0, 0, 1));
-        roomShader.setMat4("model", model);
+        roomShader.setMat4("model", sceneModel);
         roomShader.setMat4("view", view);
         roomShader.setMat4("projection", projection);
-        myRoom.Draw(roomShader);
+        shader.setFloat("bloomexp", bloomexp);
+        shader.setFloat("bloommuti", bloommuti);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, depthMap);
+
+        //drawBuffers[0] = GL_COLOR_ATTACHMENT1;
+        //glDrawBuffers(1, drawBuffers);
+        //myRoom.Draw(roomShader);
 
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glDisable(GL_DEPTH_TEST); 
-
         glClear(GL_COLOR_BUFFER_BIT);
 
         sceneShader.use();
-        glBindVertexArray(quadVAO);
-        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+       
 
+        glBindVertexArray(quadVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer); 
+        sceneShader.setInt("characterTexture", 0); 
+
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
@@ -243,11 +384,15 @@ int main()
 
         {
             static float f = 0.0f;
-            static int counter = 0;
 
-            ImGui::Begin("Test"); // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Parameter Controller"); 
+
+            ImGui::Text("Shading Style");
+            ImGui::Checkbox("Blinn-Phong", &normalShading);
+            if (normalShading) toonShading = false;
+            ImGui::Checkbox("Toon Shading", &toonShading);
+            if (toonShading) normalShading = false;
             ImGui::Text("Light Position");
-
             ImGui::SliderFloat("Light Position X", &lightPos.x,-100.0f,100.0f);
             ImGui::SliderFloat("Light Position Y", &lightPos.y, -100.0f, 100.0f);
             ImGui::SliderFloat("Light Position Z", &lightPos.z, -100.0f, 100.0f);
@@ -257,6 +402,10 @@ int main()
             ImGui::SliderFloat("RimColor Y", &rim.y, 0.0f, 1.0f);
             ImGui::SliderFloat("RimColor Z", &rim.z, 0.0f, 1.0f);
             ImGui::SliderFloat("RimColor A", &rim.a, 0.0f, 1.0f);
+
+            ImGui::Text("Bloom");
+            ImGui::SliderFloat("Bloom Exp", &bloomexp, 0.0f, 10.0f);
+            ImGui::SliderFloat("Bloom Muti", &bloommuti, 0.0f, 10.0f);
 
             ImGui::Text("Scale");
             ImGui::SliderFloat("Scale X", &scale_x, -1.0f, 1.0f);
@@ -276,11 +425,22 @@ int main()
             ImGui::SliderFloat("Split Y", &split_y, 0.0f, 1.0f);
 
             ImGui::Text("Square");
-            ImGui::SliderFloat("Square Num", &square_num, 0.0f, 10.0f);
+            ImGui::SliderFloat("Square Num", &square_num, 0.0f, 100.0f);
             ImGui::SliderFloat("Square Scale", &square_scale, 0.0f, 1.0f);
 
             ImGui::Text("Specular");
             ImGui::SliderFloat("Specular Scale", &specular_scale, 0.0f, 1.0f);
+
+            ImGui::Text("Rig");
+            ImGui::SliderFloat("Anisotropy", &anis, 0.0f, 0.99f);
+            ImGui::SliderFloat("Sharpness", &sharp, 0.0f, 1.0f);
+
+            ImGui::SliderFloat("wx", &wx, -0.5f, 0.5f);
+            ImGui::SliderFloat("wy", &wy, -0.1f, 0.1f);
+            ImGui::SliderFloat("theta_r", &theta_r, -180.0f, 180.0f);
+            ImGui::SliderFloat("r", &r, -0.0f, 1.0f);
+            ImGui::SliderFloat("G", &G, -10.0f, 10.0f);
+            ImGui::SliderFloat("d", &d, -10.0f, 10.0f);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
@@ -297,8 +457,9 @@ int main()
     glDeleteVertexArrays(1, &quadVAO);
     glDeleteBuffers(1, &quadVBO);
     glDeleteRenderbuffers(1, &rbo);
-    glDeleteFramebuffers(1, &framebuffer);
 
+    glDeleteFramebuffers(1, &framebuffer);
+    glDeleteFramebuffers(1, &depthMap);
     glfwTerminate();
     return 0;
  }
@@ -323,6 +484,7 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(UP, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
         camera.ProcessKeyboard(DOWN, deltaTime);
+    
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -338,22 +500,26 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-    if (firstMouse)
+    if (glfwGetMouseButton(window, 1) == GLFW_PRESS) 
     {
+        float xpos = static_cast<float>(xposIn);
+        float ypos = static_cast<float>(yposIn);
+        if (firstMouse)
+        {
+            lastX = xpos;
+            lastY = ypos;
+            firstMouse = false;
+        }
+
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
         lastX = xpos;
         lastY = ypos;
-        firstMouse = false;
+
+        camera.ProcessMouseMovement(xoffset, yoffset);
     }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
+    
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
@@ -369,7 +535,6 @@ unsigned int loadTexture(char const* path)
 {
     unsigned int textureID;
     glGenTextures(1, &textureID);
-
     int width, height, nrComponents;
     unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
     if (data)
@@ -402,7 +567,6 @@ unsigned int loadTexture(char const* path)
     return textureID;
 }
 
-
 unsigned int loadCubemap(vector<std::string> faces)
 {
     unsigned int textureID;
@@ -432,3 +596,4 @@ unsigned int loadCubemap(vector<std::string> faces)
 
     return textureID;
 }
+
